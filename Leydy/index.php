2edf031649469f741fa7d348045aaa2f48f79231
Leydy Reyes <l21250011@smartin.tecnm.mx>
2025-02-11 18:14:48 -0600

    deportes

--- Leydy/index.php
+++ Leydy/index.php
@@ -1,6 +1,7 @@
 <?php
-echo "LEYDY MAIRU <br>";
-echo "LEYDY MAIRU <br>";
-echo "LEYDY MAIRU <br>";
-
+echo "DEPORTES";
+echo "Futbol <br>";
+echo "Basquetbol <br>";
+echo "Voleibol <br>";
+echo "Beisbol <br>";
 
