a790477504af88df0af53d54ecfa4fdcf24255f8
Joselin Cortes Sanchez <l21250003@smartin.tecnm.mx>
2025-02-11 18:33:43 -0600

    Ejemplo

--- Jose/index.php
+++ Jose/index.php
@@ -1,26 +1,23 @@
-<?php
 <!DOCTYPE html>
 <html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Formulario de Nombre</title>
+    <title>Formulario</title>
 </head>
 <body>
-
     <form method="post" action="">
         <label for="nombre">Introduce tu nombre:</label>
         <input type="text" id="nombre" name="nombre" required>
         <input type="submit" value="Enviar">
     </form>
-
     <?php
     // Verificar si se ha enviado el formulario
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Obtener el nombre del formulario
         $nombre = htmlspecialchars($_POST['nombre']);
         // Mostrar un saludo
-        echo "<h2>Hola, $nombre! Bienvenido a nuestra página.</h2>";
+        echo "<h2>Hola, $nombre! Bienvenido</h2>";
     }
     ?>
 </body>
